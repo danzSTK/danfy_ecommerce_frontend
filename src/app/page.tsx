c1eb3ef8da0bba1f01fc114d47a5726edee8afa8
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2025-06-22 18:47:45 -0300

    feat(page): reorganização da estrutura da página inicial
    - Removido o conteúdo padrão da página inicial.
    - Adicionado o componente CarouselDestaque para exibir destaques.
    - Integrado o Sidebar e as seções de categorias para homens e mulheres.
    - Implementado um novo componente SectionDestaques.
    - Atualizada a lista de logos de marcas parceiras para serem exibidas em um carrossel.
    
    Essas alterações melhoram a experiência do usuário ao fornecer uma interface mais limpa e organizada, destacando produtos e marcas de forma mais eficaz. A nova estrutura facilita a navegação e a visualização dos conteúdos principais da aplicação.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,64 @@
+"use client";
+import { NavContentType } from "@/components/nav-menu/navMenu";
+import CarouselDestaque from "@/components/sections/CarouselDestaque";
+import { Sidebar } from "@/components/sidebar/siderbar";
+import { House, Mars, Package, Venus } from "lucide-react";
+
+import SectionCategoryMen from "@/view/home/sectionCategoryForMen";
+import SectionCategoryWomen from "@/view/home/sectionCategoryForWomen";
+import { CarouselInfinity } from "@/components/carousel/CarouselInfint";
+
+export const navigationMenuItems: NavContentType[] = [
+  { label: "Home", href: "/", icon: <House /> },
+  { label: "All Product", href: "/products", icon: <Package /> },
+  { label: "Women", href: "/services/categories/women", icon: <Venus /> },
+  { label: "Men", href: "/categories/men", icon: <Mars /> },
+];
+
+import nikeLogo from "../../public/images/logos-parceiras/Logo_NIKE.svg";
+import umbroLogo from "../../public/images/logos-parceiras/Umbro_logo.svg";
+import LouisLogo from "../../public/images/logos-parceiras/Louis_Vuitton_logo_and_wordmark.svg";
+import dolceGabanna from "../../public/images/logos-parceiras/Dolce_&_Gabbana.svg";
+import adidasLogo from "../../public/images/logos-parceiras/Adidas_Logo.svg";
+import filaLogo from "../../public/images/logos-parceiras/fila-logo.png";
+import pulmaLogo from "../../public/images/logos-parceiras/puma-logo.png";
 import Image from "next/image";
+import SectionDestaques from "@/view/home/sectionDestaques";
+
+//TODO: achar outra forma de compartilhar esse component de carouselInfinito
+//TODO: achar outra forma de compartilhar essas imagens em destaque se nao ela vai ficar hight codado entao
+
 
 export default function Home() {
+  const logoMarcas = [
+    nikeLogo,
+    umbroLogo,
+    LouisLogo,
+    dolceGabanna,
+    adidasLogo,
+    filaLogo,
+    pulmaLogo,
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <CarouselDestaque />
+      <Sidebar />
+      <SectionCategoryMen />
+      <SectionCategoryWomen />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <section>
+        <ul className="relative flex h-24 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          {logoMarcas.map((logo, i) => (
+            <CarouselInfinity key={i} index={i} length={logoMarcas.length}>
+              <div className="w-24 object-cover ">
+                <Image className="object-contain" src={logo} alt={""} fill />
+              </div>
+            </CarouselInfinity>
+          ))}
+        </ul>
+      </section>
+
+      <SectionDestaques />
+    </>
   );
 }
