b08cf15292b964ff15eb16254999020ef15aca81
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2025-07-14 20:01:25 -0300

    feat(layout): refatorar o layout da aplicação
    - Removido o uso de fontes personalizadas (Poppins, Inter, Roboto_Mono) do arquivo layout.tsx.
    - Substituído o componente Header e o SidebarProvider por BaseLayout.
    - Atualizada a exportação de metadata para utilizar defaultMetadata.
    
    Essas mudanças simplificam a estrutura do layout, melhorando a legibilidade do código e facilitando a manutenção futura. A utilização do BaseLayout proporciona uma base mais consistente para o layout da aplicação.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,52 +1,15 @@
-import type { Metadata } from "next";
-import { Inter, Poppins, Roboto_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header/header";
-import { SidebarProvider } from "@/hooks/useSidebarContext";
 
-const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  display: "swap",
-});
+import { defaultMetadata } from "@/lib/metadata";
+import BaseLayout from "@/components/baseLayout/BaseLayout";
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  display: "swap",
-});
-
-const monoSpace = Roboto_Mono({
-  variable: "--font-mono-space",
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
-  display: "swap",
-});
-
-export const metadata: Metadata = {
-  title: "Danfy Shop",
-};
+export const metadata = defaultMetadata;
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="pt-BR"
-      className={` ${poppins.variable} ${inter.variable} ${monoSpace.variable}`}
-    >
-      <body className={`dark`}>
-        <main>
-          <SidebarProvider>
-            <Header />
-            {children}
-          </SidebarProvider>
-        </main>
-      </body>
-    </html>
-  );
+  console.log("layout padrao carregado");
+  return <BaseLayout>{children}</BaseLayout>;
 }
