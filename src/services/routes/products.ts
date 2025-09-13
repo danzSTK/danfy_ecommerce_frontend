2af6bcc487c8bc54ebfb5e3100159f20cce119c3
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2025-09-13 15:17:54 -0300

    feat(products): adicionar tratamento de erros e armazenamento local para produtos
    - Adicionada a função onQueryStarted na consulta getOneProduct para armazenar o último produto visualizado no localStorage.
    - Implementado tratamento específico para erros 404 e 401, com mensagens de log apropriadas.
    - Mantido o cache dos dados não utilizados por 120 segundos.
    
    Essas alterações melhoram a experiência do usuário ao permitir que o último produto visualizado seja facilmente acessado e ao fornecer um tratamento de erros mais robusto, garantindo que os usuários sejam informados sobre problemas de acesso e disponibilidade de produtos.

--- src/services/routes/products.ts
+++ src/services/routes/products.ts
@@ -1,4 +1,5 @@
-import { ProductResponseInterface } from "@/interfaces/product";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ProductResponseInterface } from "@/interfaces/Product";
 import { api } from "../api";
 
 export const productsApi = api.injectEndpoints({
@@ -16,6 +17,29 @@ export const productsApi = api.injectEndpoints({
       query: (productId) => ({
         url: `/products/${productId}`,
       }),
+      async onQueryStarted(queryArgument, { queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          localStorage.setItem("lastViewedProduct", JSON.stringify(data));
+        } catch (error: any) {
+          if (error?.error?.status === 404) {
+            // Handle 404 error specifically
+            console.log("Product not found");
+            throw new Error("Product not found", { cause: error });
+          }
+
+          if (error?.error?.status === 401) {
+            // Handle 401 error specifically
+            console.log("Unauthorized access - perhaps redirect to login");
+            throw new Error("Unauthorized access", { cause: error });
+          }
+
+          // Handle other errors
+          console.log("An unexpected error occurred", error);
+          throw new Error("An unexpected error occurred", { cause: error });
+        }
+      },
+      keepUnusedDataFor: 120, // tempo em segundos que os dados ficam em cache após não serem mais usados
     }),
   }),
   overrideExisting: false,
