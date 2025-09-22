6f235e96b0e717432bd0bc2c99c4a91a1be48535
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2025-09-22 12:38:08 -0300

    feat(user): implementar API para gerenciamento de usuários
    - Adicionada a função `getUserById` para buscar usuário por ID.
    - Implementada a mutação `registerUser` para registrar novos usuários.
    - Criada a mutação `verifyEmail` para verificar o e-mail do usuário.
    - Adicionada a mutação `resendVerificationEmail` para reenvio de e-mail de verificação.
    
    Essas alterações introduzem uma nova API para gerenciamento de usuários, permitindo operações essenciais como registro, verificação de e-mail e recuperação de informações do usuário. Isso melhora a funcionalidade do sistema, facilitando a interação do usuário com a plataforma.

---
+++ src/services/routes/User.ts
@@ -0,0 +1,47 @@
+import { ICretedUserRequest, IUser } from "@/interfaces/AuthInterface";
+import { api } from "../api";
+
+export const UserAPI = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getUserById: builder.query<IUser, string>({
+      query: (userId) => ({
+        url: `/user/${userId}`,
+        method: "GET",
+      }),
+    }),
+
+    registerUser: builder.mutation<IUser, ICretedUserRequest>({
+      query: (userData) => ({
+        url: "user/register",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+
+    verifyEmail: builder.mutation<{ message: string }, { token: string }>({
+      query: ({ token }) => ({
+        url: `user/verify-email?token=${token}`,
+        method: "GET",
+      }),
+    }),
+
+    resendVerificationEmail: builder.mutation<
+      { message: string },
+      { email: string }
+    >({
+      query: ({ email }) => ({
+        url: "user/resend-verification-email",
+        method: "POST",
+        body: { email },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetUserByIdQuery,
+  useLazyGetUserByIdQuery,
+  useRegisterUserMutation,
+  useVerifyEmailMutation,
+  useResendVerificationEmailMutation,
+} = UserAPI;
